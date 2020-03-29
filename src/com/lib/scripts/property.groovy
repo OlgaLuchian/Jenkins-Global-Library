268178b0803d8ec72f7e0d4b52154e41725e86bb
Farkhod <sadykovfarkhod@gmail.com>
2020-03-29 02:24:49 -0500

    Deleted scripts and ready to create PR

--- src/com/lib/scripts/property.groovy
+++
@@ -1,5 +0,0 @@
-properties([[$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
-parameters([
-    extendedChoice(bindings: '', description: 'Please select docker image to deploy', descriptionPropertyValue: '', groovyClasspath: '', groovyScript: 'return ["Hello"]', multiSelectDelimiter: ',', name: 'selectedDockerImage', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', visibleItemCount: 5)
-    
-    ])])
