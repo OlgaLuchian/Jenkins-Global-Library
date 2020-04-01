168872de2e2c3a94469c848d54f80181ba9c6cf7
Farkhod <sadykovfarkhod@gmail.com>
2020-03-31 20:21:18 -0500

    Comented all stages and new variables

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -4,70 +4,62 @@ import groovy.json.JsonSlurper
 import static groovy.json.JsonOutput.*
 import hudson.FilePath
 
-// Getting userid https://stackoverflow.com/questions/35902664/get-username-logged-in-jenkins-from-jenkins-workflow-pipeline-plugin
-@NonCPS
-def getBuildUser() {
-      try {
-        return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
-      } catch (e) {
-        def user = "AutoTrigger"
-        return user
-      }
-  }
+
 
 def runPipeline() {
   def common_docker = new JenkinsDeployerPipeline()
   def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
   def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
-  def deploymentName = "${JOB_NAME}"
-                        .split('/')[0]
-                        .replace('-fuchicorp', '')
-                        .replace('-build', '')
-                        .replace('-deploy', '')
+  def allEnvironments = ['dev', 'qa', 'test', 'prod']
   def findDockerImageScript = '''
-  import groovy.json.JsonSlurper
-  def findDockerImages(branchName) {
-  def versionList = []
-  def token       = ""
-  def myJsonreader = new JsonSlurper()
-  def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
-  nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
-  while (true) {
-      if (nexusData.continuationToken) {
-      token = nexusData.continuationToken
-      nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
-      nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
-      }
-      if (nexusData.continuationToken == null ) { break } }
-  if(!versionList) { versionList.add("ImmageNotFound") } 
-  return versionList.reverse(true) }
-  findDockerImages('%s')
+    import groovy.json.JsonSlurper
+    def findDockerImages(branchName) {
+    def versionList = []
+    def token       = ""
+    def myJsonreader = new JsonSlurper()
+    def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
+    nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
+    while (true) {
+        if (nexusData.continuationToken) {
+        token = nexusData.continuationToken
+        nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
+        nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
+        }
+        if (nexusData.continuationToken == null ) { break } }
+    if(!versionList) { versionList.add("ImmageNotFound") } 
+    return versionList.reverse(true) }
+    findDockerImages('%s')
   '''
+  def deploymentName = "${JOB_NAME}".split('/')[0].replace('-fuchicorp', '').replace('-build', '').replace('-deploy', '')
 
   try {
+    // Trying to build the job
     properties([ parameters([
-      // This hard coded params should be configured inside code
+
+      // Boolean Paramater for terraform apply or not 
       booleanParam(defaultValue: false, 
       description: 'Apply All Changes', 
       name: 'terraform_apply'),
 
-
+      // Boolean Paramater for terraform destroy 
       booleanParam(defaultValue: false, 
       description: 'Destroy deployment', 
       name: 'terraform_destroy'),
 
-
+      // ExtendedChoice Script is getting all jobs based on this application
       extendedChoice(bindings: '', description: 'Please select docker image to deploy', 
       descriptionPropertyValue: '', groovyClasspath: '', 
       groovyScript:  String.format(findDockerImageScript, deploymentName) , multiSelectDelimiter: ',', 
       name: 'selectedDockerImage', quoteValue: false, 
       saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', 
       visibleItemCount: 5),
-
+      
+      // list of environment getting from <allEnvironments> and defining variable <environment> to deploy 
       choice(name: 'environment', 
-      choices: ['dev', 'qa', 'test', 'prod'], 
+      choices: allEnvironments, 
       description: 'Please select the environment to deploy'),
 
+      // Extra configurations to deploy with it 
       text(name: 'deployment_tfvars', 
       defaultValue: 'extra_values = "tools"', 
       description: 'terraform configuration')
@@ -75,7 +67,7 @@ def runPipeline() {
       ]
       )])
 
-      // Pod slave for Jenkins so Jenkins master can run the job on slaves
+      // Jenkins slave to build this job 
       def slavePodTemplate = """
       metadata:
         labels:
@@ -133,6 +125,8 @@ def runPipeline() {
       node(k8slabel) {
 
         stage("Deployment Info") {
+
+          // Colecting information to show on stage <Deployment Info>
           println(prettyPrint(toJson([
             "Environment" : environment,
             "Deployment" : deploymentName,
@@ -148,20 +142,19 @@ def runPipeline() {
           }
 
           stage('Generate Configurations') {
-            // sh "sleep 200"
             sh """
-            mkdir -p ${WORKSPACE}/deployments/terraform/
-            cat  /etc/secrets/service-account/credentials.json > ${WORKSPACE}/deployments/terraform/fuchicorp-service-account.json
-            ls ${WORKSPACE}/deployments/terraform/
-            ## This script should move to docker container to set up ~/.kube/config
-            sh /scripts/Dockerfile/set-config.sh
+              mkdir -p ${WORKSPACE}/deployments/terraform/
+              cat  /etc/secrets/service-account/credentials.json > ${WORKSPACE}/deployments/terraform/fuchicorp-service-account.json
+              ls ${WORKSPACE}/deployments/terraform/
+              ## This script should move to docker container to set up ~/.kube/config
+              sh /scripts/Dockerfile/set-config.sh
             """
             // sh /scripts/Dockerfile/set-config.sh Should go to Docker container CMD so we do not have to call on slave 
             deployment_tfvars += """
-            deployment_name        = \"${deploymentName}\"
-            deployment_environment = \"${environment}\"
-            deployment_image       = \"docker.fuchicorp.com/${selectedDockerImage}\"
-            credentials            = \"./fuchicorp-service-account.json\"
+              deployment_name        = \"${deploymentName}\"
+              deployment_environment = \"${environment}\"
+              deployment_image       = \"docker.fuchicorp.com/${selectedDockerImage}\"
+              credentials            = \"./fuchicorp-service-account.json\"
             """.stripIndent()
 
             writeFile(
@@ -176,11 +169,12 @@ def runPipeline() {
                     file(credentialsId: "academy-config", variable: 'default_config')
                 ]) {
                     sh """
-                    #!/bin/bash
-                    cat \$default_config >> ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
-                    echo #############################################################
-                    cat ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
-                    echo #############################################################"""
+                      #!/bin/bash
+                      cat \$default_config >> ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
+                      echo #############################################################
+                      cat ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
+                      echo #############################################################
+                    """
                 }
             
                 println("Found default configurations appanded to main configuration")
@@ -197,8 +191,9 @@ def runPipeline() {
                 dir("${WORKSPACE}/deployments/terraform/") {
                   echo "##### Terraform Applying the Changes ####"
                   sh '''#!/bin/bash -e
-                  source set-env.sh deployment_configuration.tfvars
-                  terraform apply --auto-approve -var-file=deployment_configuration.tfvars'''
+                    source set-env.sh deployment_configuration.tfvars
+                    terraform apply --auto-approve -var-file=deployment_configuration.tfvars
+                  '''
                 }
 
               } else {
@@ -206,8 +201,8 @@ def runPipeline() {
                 dir("${WORKSPACE}/deployments/terraform/") {
                   echo "##### Terraform Plan (Check) the Changes #### "
                   sh '''#!/bin/bash -e
-                  source set-env.sh deployment_configuration.tfvars
-                  terraform plan -var-file=deployment_configuration.tfvars
+                    source set-env.sh deployment_configuration.tfvars
+                    terraform plan -var-file=deployment_configuration.tfvars
                   '''
                 }
               }
@@ -221,8 +216,9 @@ def runPipeline() {
                   dir("${WORKSPACE}/deployments/terraform/") {
                     echo "##### Terraform Destroing ####"
                     sh '''#!/bin/bash -e
-                    source set-env.sh deployment_configuration.tfvars
-                    terraform destroy --auto-approve -var-file=deployment_configuration.tfvars'''
+                      source set-env.sh deployment_configuration.tfvars
+                      terraform destroy --auto-approve -var-file=deployment_configuration.tfvars
+                    '''
                   }
                 } else {
                   println("""
@@ -239,7 +235,7 @@ def runPipeline() {
              if (params.terraform_apply) {
                println("""
 
-               Sorry you can not destroy and apply at the same time
+                Sorry you can not destroy and apply at the same time
 
                """)
                currentBuild.result = 'FAILURE'
@@ -256,6 +252,15 @@ def runPipeline() {
   }
 }
 
-
+// Function to get user id 
+@NonCPS
+def getBuildUser() {
+      try {
+        return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+      } catch (e) {
+        def user = "AutoTrigger"
+        return user
+      }
+  }
 
 return this
