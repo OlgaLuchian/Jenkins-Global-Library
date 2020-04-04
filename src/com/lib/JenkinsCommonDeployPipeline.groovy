8ce49cda18dd449afa0c5288c5c12b71fbee7049
Farkhod <sadykovfarkhod@gmail.com>
2020-04-04 18:10:20 -0500

    moved common functions

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -8,7 +8,7 @@ import hudson.FilePath
 
 def runPipeline() {
   def common_docker   = new JenkinsDeployerPipeline()
-  def commonFunction  = new commonFunction()
+  def commonFunction  = new CommonFunctions()
   def triggerUser     = getBuildUser()
   def branch          = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
   def k8slabel        = "jenkins-pipeline-${UUID.randomUUID().toString()}"
